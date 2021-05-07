844bdf17429d16033100eb08747cc9118a7482fe
fatih40 <baserfatih40@gmail.com>
2021-05-08 01:41:36 +0200

    Initial commit

---
+++ app/src/main/java/com/fatihbaser/add_list_room/MainActivity.java
@@ -0,0 +1,33 @@
+package com.fatihbaser.add_list_room;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btAdd, btReset;
+    RecyclerView recyclerView;
+
+    List<MainData> dataLsit=new ArrayList<>();
+  
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText=findViewById(R.id.edit_text);
+        btAdd=findViewById(R.id.bt_add);
+        btReset=findViewById(R.id.bt_reset);
+        recyclerView=findViewById(R.id.recycler_view);
+
+
+    }
+}
